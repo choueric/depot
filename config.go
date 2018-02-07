fa7658db0d1bcee7570f5814ca91a0e9722b2872
choueric <zhssmail@gmail.com>
2018-02-07 15:41:47 +0800

    add control_port in configuration

--- config.go
+++ config.go
@@ -8,13 +8,14 @@ import (
 )
 
 type Config struct {
-	ServerPort int    `json:"server_port"`
-	ServerAddr string `json:"server_addr"`
-	TunnelPort int    `json:"tunnel_port"`
-	UserName   string `json:"user_name"`
-	Password   string `json:"password"`
-	Timeout    int    `json:"timeout"` // unit: second
-	Debug      bool   `json:"debug"`
+	ServerAddr  string `json:"server_addr"`
+	ServerPort  int    `json:"server_port"`
+	ControlPort int    `json:"control_port"`
+	TunnelPort  int    `json:"tunnel_port"`
+	UserName    string `json:"user_name"`
+	Password    string `json:"password"`
+	Timeout     int    `json:"timeout"` // unit: second
+	Debug       bool   `json:"debug"`
 	// internal
 	jc   interface{} // must be interface{}, otherwise panic
 	path string      // path of the configuration file
@@ -23,7 +24,8 @@ type Config struct {
 const defaultConfig = `{
 	"server_addr": "127.0.0.1",
 	"server_port": 8864,
-	"tunnel_port": 8964,
+	"control_port": 8964,
+	"tunnel_port": 9064,
 	"timeout": 600,
 	"user_name": "user",
 	"password": "password",
